a4a1b61decc33901a7bbb8b4ad1ef8f2f8019677
Marshi00 <112508169+Marshi00@users.noreply.github.com>
2024-12-03 06:25:55 -0500

    tag_details Script CSV to DB

--- Destination_DB_Setup/tag_details.sql
+++ Destination_DB_Setup/tag_details.sql
@@ -1,23 +1,28 @@
--- Table: public.tag_details
-
--- DROP TABLE IF EXISTS public.tag_details;
-
-CREATE TABLE IF NOT EXISTS public.tag_details
-(
-    id integer NOT NULL DEFAULT nextval('tag_details_id_seq'::regclass),
-    tagname character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    description text COLLATE pg_catalog."default",
-    unit character varying(20) COLLATE pg_catalog."default",
-    path text COLLATE pg_catalog."default" NOT NULL,
-    type character varying(10) COLLATE pg_catalog."default",
-    device character varying(100) COLLATE pg_catalog."default",
-    location character varying(100) COLLATE pg_catalog."default",
-    CONSTRAINT tag_details_pkey PRIMARY KEY (id),
-    CONSTRAINT tag_details_path_key UNIQUE (path),
-    CONSTRAINT tag_details_tagname_key UNIQUE (tagname)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.tag_details
-    OWNER to postgres;
+CREATE TABLE SampleTable (
+    tagname VARCHAR(50),
+    description TEXT,
+    short_description VARCHAR(255),
+    unit VARCHAR(10),
+    path TEXT,
+    aggregation VARCHAR(50),
+    site VARCHAR(50),
+    type VARCHAR(50),
+    manufacture VARCHAR(100),
+    model_number VARCHAR(100),
+    calculated_flow VARCHAR(3),
+    water_use VARCHAR(3),
+    water_model VARCHAR(3),
+    thermal_model VARCHAR(3),
+    operations_all VARCHAR(3),
+    operations_water VARCHAR(3),
+    operations_wastewater VARCHAR(3),
+    regulation_water VARCHAR(3),
+    regulation_wastewater VARCHAR(3),
+    maintenance VARCHAR(3),
+    Health_and_Safety VARCHAR(3),
+    Building_Condition VARCHAR(3),
+    Placeholder_1 VARCHAR(3),
+    Placeholder_2 VARCHAR(3),
+    Placeholder_3 VARCHAR(3),
+    Placeholder_4 VARCHAR(3)
+);
